39576934936fe7ee04e9194c5c354bfd3a320d80
Jonathan Panning <ikonmobile@gmail.com>
2014-07-28 14:41:09 -0600

    More methods to help testers
    This is duplicated from SSRSUtil but I wanted to use these specifically
    for testers.

--- SSRSMigrate/SSRSMigrate.TestHelper/TestUtils.cs
+++ SSRSMigrate/SSRSMigrate.TestHelper/TestUtils.cs
@@ -22,5 +22,24 @@ namespace SSRSMigrate.TestHelper
 
             return doc.InnerXml;
         }
+
+        public static byte[] StringToByteArray(string text)
+        {
+            char[] charArray = text.ToCharArray();
+
+            byte[] byteArray = new byte[charArray.Length];
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                byteArray[i] = Convert.ToByte(charArray[i]);
+            }
+
+            return byteArray;
+        }
+
+        public static string ByteArrayToString(byte[] byteArray)
+        {
+            return Encoding.UTF8.GetString(byteArray);
+        }
     }
 }
