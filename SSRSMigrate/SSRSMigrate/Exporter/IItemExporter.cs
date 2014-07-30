ed71c3f8c55ad57398ab784c5154f715f561d544
Jonathan Panning <ikonmobile@gmail.com>
2014-07-30 16:41:22 -0600

    ItemExporter interface

---
+++ SSRSMigrate/SSRSMigrate/Exporter/IItemExporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SSRSMigrate.SSRS.Item;
+
+namespace SSRSMigrate.Exporter
+{
+    public interface IItemExporter<T> where T : ReportServerItem
+    {
+        ExportStatus SaveItem(T item, string fileName);
+    }
+}
