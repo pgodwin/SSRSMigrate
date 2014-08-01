e4af3446c5a9bcb10c00afdb2da80ef07bdfc2e6
jpann <ikonmobile@gmail.com>
2014-07-31 19:24:55 -0600

    Start ReportItemExporter implementation

---
+++ SSRSMigrate/SSRSMigrate/Exporter/ReportItemExporter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SSRSMigrate.SSRS.Item;
+using System.IO;
+using System.Xml;
+
+namespace SSRSMigrate.Exporter
+{
+    public class ReportItemExporter : IItemExporter<ReportItem>
+    {
+        public ReportItemExporter()
+        {
+        }
+
+        public ExportStatus SaveItem(ReportItem item, string fileName, bool overwrite = true)
+        {
+            if (item == null)
+                throw new ArgumentNullException("item");
+
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentException("fileName");
+
+            try
+            {
+                if (File.Exists(fileName) && !overwrite)
+                    throw new IOException(string.Format("File '{0}' already exists.", fileName));
+                else if (File.Exists(fileName) && overwrite)
+                    File.Delete(fileName);
+
+                if (!Directory.Exists(Path.GetDirectoryName(fileName)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+
+                XmlDocument doc = new XmlDocument();
+
+                using (MemoryStream oStream = new MemoryStream(item.Definition))
+                {
+                    doc.Load(oStream);
+                    doc.Save(fileName);
+                }
+            }
+            catch (Exception er)
+            {
+                return new ExportStatus(fileName, item.Path, new string[] { er.Message }, false);
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
