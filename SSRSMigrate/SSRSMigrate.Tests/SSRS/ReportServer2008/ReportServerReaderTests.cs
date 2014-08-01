7399e35933d5f3e7d14bf65de3bb769e75856d65
jpann <ikonmobile@gmail.com>
2014-08-01 12:02:22 -0600

    Add general ReportServerReader2008 constructor test

---
+++ SSRSMigrate/SSRSMigrate.Tests/SSRS/ReportServer2008/ReportServerReaderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SSRSMigrate.SSRS.Reader;
+
+namespace SSRSMigrate.Tests.SSRS.ReportServer2008
+{
+    [TestFixture]
+    class ReportServerReaderTests
+    {
+        [Test]
+        public void ReportServerReader_NullRepository()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(
+                delegate
+                {
+                    ReportServerReader reader = new ReportServerReader(null);
+                });
+
+            Assert.That(ex.Message, Is.EqualTo("Value cannot be null.\r\nParameter name: repository"));
+        
+        }
+    }
+}
