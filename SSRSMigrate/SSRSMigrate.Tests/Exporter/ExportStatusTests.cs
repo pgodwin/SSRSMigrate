55ddcc15a53a997cac51cdf7da0fe39fef709b44
jpann <ikonmobile@gmail.com>
2014-08-07 18:59:53 -0600

    ExportStatus tests

---
+++ SSRSMigrate/SSRSMigrate.Tests/Exporter/ExportStatusTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SSRSMigrate.Exporter;
+
+namespace SSRSMigrate.Tests.Exporter
+{
+    [TestFixture]
+    [CoverageExcludeAttribute]
+    class ExportStatusTests
+    {
+        [Test]
+        public void Succeed()
+        {
+            string expectedToPath = "C:\\temp\\SSRSMigrate_Tests\\Reports";
+            string expectedFromPath = "/SSRSMigrate_Tests/Reports";
+            bool expectedSuccess = true;
+            string[] expectedErrors = null;
+
+            ExportStatus actualStatus = new ExportStatus(expectedToPath, expectedFromPath, expectedErrors, expectedSuccess);
+
+            Assert.AreEqual(expectedToPath, actualStatus.ToPath);
+            Assert.AreEqual(expectedFromPath, actualStatus.FromPath);
+            Assert.AreEqual(expectedErrors, actualStatus.Errors);
+            Assert.AreEqual(expectedSuccess, actualStatus.Success);
+        }
+
+        [Test]
+        public void WithErrors()
+        {
+            string expectedToPath = "C:\\temp\\SSRSMigrate_Tests\\Reports";
+            string expectedFromPath = "/SSRSMigrate_Tests/Reports";
+            bool expectedSuccess = false;
+            string[] expectedErrors = new string[] { "Permission denied." };
+
+            ExportStatus actualStatus = new ExportStatus(expectedToPath, expectedFromPath, expectedErrors, expectedSuccess);
+
+            Assert.AreEqual(expectedToPath, actualStatus.ToPath);
+            Assert.AreEqual(expectedFromPath, actualStatus.FromPath);
+            Assert.AreEqual(expectedErrors, actualStatus.Errors);
+            Assert.AreEqual(expectedSuccess, actualStatus.Success);
+        }
+
+        [Test]
+        public void NullToPath()
+        {
+            string expectedFromPath = "/SSRSMigrate_Tests/Reports";
+            bool expectedSuccess = true;
+            string[] expectedErrors = null;
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate
+                {
+                    ExportStatus actualStatus = new ExportStatus(null, expectedFromPath, expectedErrors, expectedSuccess);
+                });
+
+            Assert.That(ex.Message, Is.EqualTo("pathTo"));
+        }
+
+        [Test]
+        public void EmptyToPath()
+        {
+            string expectedFromPath = "/SSRSMigrate_Tests/Reports";
+            bool expectedSuccess = true;
+            string[] expectedErrors = null;
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate
+                {
+                    ExportStatus actualStatus = new ExportStatus("", expectedFromPath, expectedErrors, expectedSuccess);
+                });
+
+            Assert.That(ex.Message, Is.EqualTo("pathTo"));
+        }
+
+        [Test]
+        public void NullFromPath()
+        {
+            string expectedToPath = "C:\\temp\\SSRSMigrate_Tests\\Reports";
+            bool expectedSuccess = true;
+            string[] expectedErrors = null;
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate
+                {
+                    ExportStatus actualStatus = new ExportStatus(expectedToPath, null, expectedErrors, expectedSuccess);
+                });
+
+            Assert.That(ex.Message, Is.EqualTo("pathFrom"));
+        }
+
+        [Test]
+        public void EmptyFromPath()
+        {
+            string expectedToPath = "C:\\temp\\SSRSMigrate_Tests\\Reports";
+            bool expectedSuccess = true;
+            string[] expectedErrors = null;
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate
+                {
+                    ExportStatus actualStatus = new ExportStatus(expectedToPath, "", expectedErrors, expectedSuccess);
+                });
+
+            Assert.That(ex.Message, Is.EqualTo("pathFrom"));
+        }
+    }
+}
