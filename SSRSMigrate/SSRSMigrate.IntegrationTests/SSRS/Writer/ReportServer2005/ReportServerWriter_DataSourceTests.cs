60e2abcb670bab8e9af4ee69cb61310462814784
jpann <ikonmobile@gmail.com>
2014-08-18 16:18:38 -0600

    Start ReportingService2005 datasource writer integration tests

--- SSRSMigrate/SSRSMigrate.IntegrationTests/SSRS/Writer/ReportServer2005/ReportServerWriter_DataSourceTests.cs
+++ SSRSMigrate/SSRSMigrate.IntegrationTests/SSRS/Writer/ReportServer2005/ReportServerWriter_DataSourceTests.cs
@@ -6,6 +6,9 @@ using NUnit.Framework;
 using Ninject;
 using SSRSMigrate.SSRS.Writer;
 using SSRSMigrate.SSRS.Item;
+using SSRSMigrate.Factory;
+using SSRSMigrate.TestHelper;
+using System.Net;
 
 namespace SSRSMigrate.IntegrationTests.SSRS.Writer.ReportServer2005
 {
@@ -15,6 +18,7 @@ namespace SSRSMigrate.IntegrationTests.SSRS.Writer.ReportServer2005
     {
         StandardKernel kernel = null;
         ReportServerWriter writer = null;
+        string outputPath = null;
 
         #region DataSourceItems
         List<DataSourceItem> dataSourceItems = null;
@@ -23,6 +27,54 @@ namespace SSRSMigrate.IntegrationTests.SSRS.Writer.ReportServer2005
         [TestFixtureSetUp]
         public void TestFixtureSetUp()
         {
+            kernel = new StandardKernel(new DependencyModule());
+
+            // SetupDataSourceItems
+            dataSourceItems = new List<DataSourceItem>()
+            {
+                new DataSourceItem()
+                {
+                    Description = null,
+                    Name = "Test Data Source",
+                    Path = "/SSRSMigrate_Tests/Test Data Source",
+                    ConnectString = "Data Source=(local);Initial Catalog=TestDatabase;Application Name=SSRSMigrate_IntegrationTest",
+                    CredentialsRetrieval ="Integrated",
+                    Enabled = true,
+                    EnabledSpecified = true,
+                    Extension = "SQL",
+                    ImpersonateUser = false,
+                    ImpersonateUserSpecified = true,
+                    OriginalConnectStringExpressionBased = false,
+                    Password = null,
+                    Prompt = "Enter a user name and password to access the data source:",
+                    UseOriginalConnectString = false,
+                    UserName = null,
+                    WindowsCredentials = false
+                },
+               new DataSourceItem()
+                {
+                    Description = null,
+                    Name = "Test 2 Data Source",
+                    Path = "/SSRSMigrate_Tests/Test 2 Data Source",
+                    ConnectString = "Data Source=(local);Initial Catalog=TestDatabase;Application Name=SSRSMigrate_IntegrationTest",
+                    CredentialsRetrieval ="Integrated",
+                    Enabled = true,
+                    EnabledSpecified = true,
+                    Extension = "SQL",
+                    ImpersonateUser = false,
+                    ImpersonateUserSpecified = true,
+                    OriginalConnectStringExpressionBased = false,
+                    Password = null,
+                    Prompt = "Enter a user name and password to access the data source:",
+                    UseOriginalConnectString = false,
+                    UserName = null,
+                    WindowsCredentials = false
+                },
+            };
+
+            outputPath = Properties.Settings.Default.DestinationPath;
+
+            writer = kernel.Get<IReportServerWriterFactory>().GetWriter<ReportServerWriter>("2005-DEST");
         }
 
         [TestFixtureTearDown]
@@ -34,20 +86,36 @@ namespace SSRSMigrate.IntegrationTests.SSRS.Writer.ReportServer2005
         [SetUp]
         public void SetUp()
         {
+            SetupEnvironment();
         }
 
         [TearDown]
         public void TearDown()
         {
+            TeardownEnvironment();
         }
 
         #region Environment Setup/Teardown
         private void SetupEnvironment()
         {
-
+            ReportingService2005Utility.SetupEnvironment(
+                Properties.Settings.Default.ReportServer2008WebServiceUrl, 
+                CredentialCache.DefaultNetworkCredentials,
+                outputPath);
         }
 
         private void TeardownEnvironment()
+        {
+            ReportingService2005Utility.TeardownEnvironment(
+                Properties.Settings.Default.ReportServer2008WebServiceUrl,
+                CredentialCache.DefaultNetworkCredentials,
+                outputPath);
+        }
+        #endregion
+
+        #region WriteDataSource Tests
+        [Test]
+        public void WriteDataSource()
         {
 
         }
