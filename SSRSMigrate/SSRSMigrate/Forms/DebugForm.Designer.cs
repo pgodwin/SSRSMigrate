8bf72fedbbe77cdd5fd67ac9051813ff1d336548
jpann <ikonmobile@gmail.com>
2014-10-04 21:28:43 -0600

    Add debugger form.

---
+++ SSRSMigrate/SSRSMigrate/Forms/DebugForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SSRSMigrate.Forms
+{
+    partial class DebugForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstDebug = new System.Windows.Forms.ListView();
+            this.colDateTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colMessage = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colException = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colCaller = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lstDebug);
+            this.groupBox1.Location = new System.Drawing.Point(8, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(787, 245);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lstDebug
+            // 
+            this.lstDebug.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstDebug.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colDateTime,
+            this.colCaller,
+            this.colMessage,
+            this.colException});
+            this.lstDebug.Location = new System.Drawing.Point(11, 16);
+            this.lstDebug.Name = "lstDebug";
+            this.lstDebug.Size = new System.Drawing.Size(765, 218);
+            this.lstDebug.TabIndex = 0;
+            this.lstDebug.UseCompatibleStateImageBehavior = false;
+            this.lstDebug.View = System.Windows.Forms.View.Details;
+            // 
+            // colDateTime
+            // 
+            this.colDateTime.Text = "Timestamp";
+            this.colDateTime.Width = 109;
+            // 
+            // colMessage
+            // 
+            this.colMessage.Text = "Message";
+            this.colMessage.Width = 422;
+            // 
+            // colException
+            // 
+            this.colException.Text = "Exception";
+            this.colException.Width = 171;
+            // 
+            // colCaller
+            // 
+            this.colCaller.Text = "Caller";
+            this.colCaller.Width = 110;
+            // 
+            // DebugForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 255);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "DebugForm";
+            this.ShowIcon = false;
+            this.Text = "Debug";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DebugForm_FormClosing);
+            this.Load += new System.EventHandler(this.DebugForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView lstDebug;
+        private System.Windows.Forms.ColumnHeader colDateTime;
+        private System.Windows.Forms.ColumnHeader colMessage;
+        private System.Windows.Forms.ColumnHeader colException;
+        private System.Windows.Forms.ColumnHeader colCaller;
+    }
+}
---
+++ SSRSMigrate/SSRSMigrate/Forms/DebugForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SSRSMigrate.Forms
+{
+    public partial class DebugForm : Form
+    {
+        #region Public Methods
+        /// <summary>
+        /// Adds a message to the debug logger.
+        /// </summary>
+        /// <param name="Message">The message text.</param>
+        public void LogMessage(string message)
+        {
+            if (message == null)
+                message = "";
+
+            StackTrace stackTrace = new StackTrace();
+            var callerMethod = new StackTrace().GetFrame(1).GetMethod();
+            var callerClass = callerMethod.ReflectedType.Name;
+            string caller = string.Format("{0}.{1}", callerClass, callerMethod.Name);
+
+            ListViewItem oItem = new ListViewItem(DateTime.Now.ToString());
+            oItem.SubItems.Add(caller);
+            oItem.SubItems.Add(message);
+            oItem.ToolTipText = message;
+
+            //lstDebug.Items.Add(oItem);
+            lstDebug.Invoke(new Action(() => lstDebug.Items.Add(oItem)));
+            lstDebug.Invoke(new Action(() => oItem.EnsureVisible()));
+        }
+
+        /// <summary>
+        /// Adds an exception to the debug logger.
+        /// </summary>
+        /// <param name="Message">The message text.</param>
+        /// <param name="Exception">The exception object.</param>
+        public void LogMessage(string message, Exception exception)
+        {
+            if (message == null)
+                message = "";
+
+            string sException = exception.GetType().ToString();
+
+            if (sException == null)
+                sException = "";
+
+            StackTrace stackTrace = new StackTrace();
+            var callerMethod = new StackTrace().GetFrame(1).GetMethod();
+            var callerClass = callerMethod.ReflectedType.Name;
+            string caller = string.Format("{0}.{1}", callerClass, callerMethod.Name);
+
+            ListViewItem oItem = new ListViewItem(DateTime.Now.ToString());
+            oItem.SubItems.Add(caller);
+            oItem.SubItems.Add(message);
+            oItem.SubItems.Add(sException);
+            oItem.ForeColor = Color.Red;
+            oItem.ToolTipText = message;
+
+            //lstDebug.Items.Add(oItem);
+            lstDebug.Invoke(new Action(() => lstDebug.Items.Add(oItem)));
+            lstDebug.Invoke(new Action(() => oItem.EnsureVisible()));
+        }
+        #endregion
+
+        public DebugForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DebugForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DebugForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.ApplicationExitCall)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+    }
+}
