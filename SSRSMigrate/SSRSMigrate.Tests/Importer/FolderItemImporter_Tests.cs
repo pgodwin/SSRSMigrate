8d6ec665e4587bddaf4a302c14b061fbbb9f33ce
jpann <ikonmobile@gmail.com>
2014-10-27 18:35:28 -0600

    Start FolderItemImporter tests

--- SSRSMigrate/SSRSMigrate.Tests/Importer/FolderItemImporter_Tests.cs
+++ SSRSMigrate/SSRSMigrate.Tests/Importer/FolderItemImporter_Tests.cs
@@ -1,18 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
+using Moq;
 using NUnit.Framework;
+using SSRSMigrate.Importer;
+using SSRSMigrate.SSRS.Item;
+using SSRSMigrate.Status;
+using SSRSMigrate.TestHelper.Logging;
 
 namespace SSRSMigrate.Tests.Importer
 {
     [TestFixture]
     class FolderItemImporter_Tests
     {
+        private FolderItemImporter importer = null;
+        private Mock<IFileSystem> fileSystemMock = null;
+        private MockLogger loggerMock = null;
+
+        #region Expected Data
+        string expectedFolderItemFileName = "C:\\temp\\SSRSMigrate_AW_Tests\\Export\\SSRSMigrate_AW_Tests\\Reports";
+
+        string expectedFolderItemPath = "C:\\temp\\SSRSMigrate_AW_Tests\\Export\\SSRSMigrate_AW_Tests";
+        
+
+        FolderItem expectedFolderItem = new FolderItem()
+                {
+                    Name = "Reports",
+                    Path = "/SSRSMigrate_AW_Tests/Reports",
+                };
+
+        #endregion
+
         [TestFixtureSetUp]
         public void TestFixtureSetUp()
         {
+            loggerMock = new MockLogger();
+            fileSystemMock = new Mock<IFileSystem>();
 
+            // ISystemIOWrapper.Directory.Exists Method Mocks
+            fileSystemMock.Setup(f => f.Directory.Exists(expectedFolderItemFileName))
+                .Returns(() => true);
+
+            // ISystemIOWrapper.Path.GetDirectoryName Method Mocks
+            fileSystemMock.Setup(f => f.Path.GetDirectoryName(expectedFolderItemFileName))
+                .Returns(() => expectedFolderItemPath);
+
+            fileSystemMock.Setup(f => f.Path.GetFileName(expectedFolderItemFileName))
+                .Returns(() => expectedFolderItem.Name);
+
+            importer = new FolderItemImporter(fileSystemMock.Object, loggerMock);
         }
 
         [TestFixtureTearDown]
@@ -32,5 +70,20 @@ namespace SSRSMigrate.Tests.Importer
         {
 
         }
+
+        #region ImportItem Tests
+
+        [Test]
+        public void ImportItem()
+        {
+            ImportStatus status = null;
+
+            FolderItem actual = importer.ImportItem(expectedFolderItemFileName, out status);
+
+            Assert.NotNull(actual);
+            Assert.AreEqual(expectedFolderItem.Name, actual.Name);
+            Assert.AreEqual(expectedFolderItem.Path, actual.Path);
+        }
+        #endregion
     }
 }
--- SSRSMigrate/SSRSMigrate/Importer/FolderItemImporter.cs
+++ SSRSMigrate/SSRSMigrate/Importer/FolderItemImporter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
+using Ninject.Extensions.Logging;
 using SSRSMigrate.SSRS.Item;
 using SSRSMigrate.Status;
 
@@ -11,18 +13,58 @@ namespace SSRSMigrate.Importer
     public class FolderItemImporter : IItemImporter<FolderItem>
     {
         private readonly IFileSystem mFileSystem = null;
-
-        public FolderItemImporter(IFileSystem fileSystem)
+        private readonly ILogger mLogger = null;
+ 
+        public FolderItemImporter(IFileSystem fileSystem,
+            ILogger logger)
         {
             if (fileSystem == null)
                 throw new ArgumentNullException("fileSystem");
 
+            if (logger == null)
+                throw new ArgumentNullException("logger");
+
             this.mFileSystem = fileSystem;
+            this.mLogger = logger;
         }
 
         public FolderItem ImportItem(string filename, out ImportStatus status)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(filename))
+                throw new ArgumentException("filename");
+
+            if (!this.mFileSystem.Directory.Exists(filename))
+                throw new DirectoryNotFoundException(filename);
+
+            string diskPath = this.mFileSystem.Path.GetDirectoryName(filename);
+            Exception error = null;
+            bool success = true;
+
+            FolderItem item = null;
+
+            try
+            {
+                string name = this.mFileSystem.Path.GetFileName(filename);
+
+                // Get the path Item's path from the path on disk, inside the Export\ directory
+                string token = "\\Export\\";
+                string path = filename.Substring(filename.IndexOf(token) + token.Length - 1).Replace("\\", "/");
+
+                item = new FolderItem();
+                item.Name = name;
+                item.Path = path;
+
+                success = true;
+            }
+            catch (Exception er)
+            {
+                success = false;
+                error = er;
+            }
+
+            status = new ImportStatus(filename, diskPath, error, success);
+
+            return item;
         }
     }
 }
