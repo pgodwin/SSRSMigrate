ac22cdcd483dec4a69646f7681bab7976f63c038
jpann <ikonmobile@gmail.com>
2014-10-31 15:43:15 -0600

    Better comments for the DataSourceItemExporter unit tests.

--- SSRSMigrate/SSRSMigrate.Tests/Exporter/DataSourceItemExporterTests.cs
+++ SSRSMigrate/SSRSMigrate.Tests/Exporter/DataSourceItemExporterTests.cs
@@ -22,11 +22,39 @@ namespace SSRSMigrate.Tests.Exporter
         Mock<IExportWriter> exportWriterMock = null;
         Mock<ISerializeWrapper> serializeWrapperMock = null;
 
-        #region Expected Values
-        DataSourceItem expectedDataSourceItem = null;
+        #region Test Data
+        DataSourceItem dataSourceItem = new DataSourceItem()
+            {
+                CreatedBy = "DOMAIN\\user",
+                CreationDate = DateTime.Parse("7/23/2014 8:28:43 AM"),
+                Description = null,
+                ID = Guid.NewGuid().ToString(),
+                ModifiedBy = "DOMAIN\\user",
+                ModifiedDate = DateTime.Parse("7/23/2014 8:28:43 AM"),
+                Size = 414,
+                VirtualPath = null,
+                Name = "AWDataSource",
+                Path = "/SSRSMigrate_AW_Tests/Data Sources/AWDataSource",
+                ConnectString = "Data Source=(local)\\SQL2008;Initial Catalog=AdventureWorks2008",
+                CredentialsRetrieval = "Integrated",
+                Enabled = true,
+                EnabledSpecified = true,
+                Extension = "SQL",
+                ImpersonateUser = false,
+                ImpersonateUserSpecified = true,
+                OriginalConnectStringExpressionBased = false,
+                Password = null,
+                Prompt = "Enter a user name and password to access the data source:",
+                UseOriginalConnectString = false,
+                UserName = null,
+                WindowsCredentials = false
+            };
+        #endregion
 
+        #region Expected Values
         string expectedDataSourceItemFileName = "C:\\temp\\SSRSMigrate_AW_Tests\\AWDataSource.json";
 
+        // The expected json string to get when serializing dataSourceItem
         string expectedDataSourceItemJson = @"{
   ""ConnectString"": ""Data Source=(local)\\SQL2008;Initial Catalog=AdventureWorks2008"",
   ""CredentialsRetrieval"": ""Integrated"",
@@ -57,34 +85,7 @@ namespace SSRSMigrate.Tests.Exporter
         [TestFixtureSetUp]
         public void TestFixtureSetUp()
         {
-            expectedDataSourceItem = new DataSourceItem()
-            {
-                CreatedBy = "DOMAIN\\user",
-                CreationDate = DateTime.Parse("7/23/2014 8:28:43 AM"),
-                Description = null,
-                ID = Guid.NewGuid().ToString(),
-                ModifiedBy = "DOMAIN\\user",
-                ModifiedDate = DateTime.Parse("7/23/2014 8:28:43 AM"),
-                Size = 414,
-                VirtualPath = null,
-                Name = "AWDataSource",
-                Path = "/SSRSMigrate_AW_Tests/Data Sources/AWDataSource",
-                ConnectString = "Data Source=(local)\\SQL2008;Initial Catalog=AdventureWorks2008",
-                CredentialsRetrieval = "Integrated",
-                Enabled = true,
-                EnabledSpecified = true,
-                Extension = "SQL",
-                ImpersonateUser = false,
-                ImpersonateUserSpecified = true,
-                OriginalConnectStringExpressionBased = false,
-                Password = null,
-                Prompt = "Enter a user name and password to access the data source:",
-                UseOriginalConnectString = false,
-                UserName = null,
-                WindowsCredentials = false
-            };
-
-            expectedDataSourceItemJson = JsonConvert.SerializeObject(expectedDataSourceItem, Formatting.Indented);
+            expectedDataSourceItemJson = JsonConvert.SerializeObject(dataSourceItem, Formatting.Indented);
             
             exportWriterMock = new Mock<IExportWriter>();
             serializeWrapperMock = new Mock<ISerializeWrapper>();
@@ -97,7 +98,7 @@ namespace SSRSMigrate.Tests.Exporter
                 .Throws(new IOException(string.Format("File '{0}' already exists.", expectedDataSourceItemFileName)));
 
             // Mock ISerializeWrapper.Serialize
-            serializeWrapperMock.Setup(s => s.SerializeObject(expectedDataSourceItem))
+            serializeWrapperMock.Setup(s => s.SerializeObject(dataSourceItem))
                 .Returns(() => expectedDataSourceItemJson);
 
             exporter = new DataSourceItemExporter(exportWriterMock.Object, serializeWrapperMock.Object);
@@ -119,6 +120,9 @@ namespace SSRSMigrate.Tests.Exporter
         }
 
         #region Constructor Tests
+        /// <summary>
+        /// Test the constructor successfully.
+        /// </summary>
         [Test]
         public void Constructor_Succeed()
         {
@@ -127,6 +131,9 @@ namespace SSRSMigrate.Tests.Exporter
             Assert.NotNull(dataSourceExporter);
         }
 
+        /// <summary>
+        /// Test the constuctor when passed a null IItemExporter parameter.
+        /// </summary>
         [Test]
         public void Constructor_NullIExportWriter()
         {
@@ -140,6 +147,9 @@ namespace SSRSMigrate.Tests.Exporter
         
         }
 
+        /// <summary>
+        /// Test the constuctor when passed a null ISerializeWrapper parameter.
+        /// </summary>
         [Test]
         public void Constructor_NullISerializeWrapper()
         {
@@ -155,28 +165,37 @@ namespace SSRSMigrate.Tests.Exporter
         #endregion
 
         #region DataSourceItemExporeter.SaveItem Tests
+        /// <summary>
+        /// Test saving a DataSourcItem successfully.
+        /// </summary>
         [Test]
         public void SaveItem()
         {
-            ExportStatus actualStatus = exporter.SaveItem(expectedDataSourceItem, expectedDataSourceItemFileName, true);
+            ExportStatus actualStatus = exporter.SaveItem(dataSourceItem, expectedDataSourceItemFileName, true);
 
             exportWriterMock.Verify(e => e.Save(expectedDataSourceItemFileName, expectedDataSourceItemJson, true));
 
             Assert.True(actualStatus.Success);
             Assert.AreEqual(expectedDataSourceItemFileName, actualStatus.ToPath);
-            Assert.AreEqual(expectedDataSourceItem.Path, actualStatus.FromPath);
+            Assert.AreEqual(dataSourceItem.Path, actualStatus.FromPath);
         }
 
+        /// <summary>
+        /// Test saving a DataSourceItem where the filename already exists on disk and overwrite is false.
+        /// </summary>
         [Test]
         public void SaveItem_FileExists_DontOverwrite()
         {
-            ExportStatus actualStatus = exporter.SaveItem(expectedDataSourceItem, expectedDataSourceItemFileName, false);
+            ExportStatus actualStatus = exporter.SaveItem(dataSourceItem, expectedDataSourceItemFileName, false);
 
             Assert.False(actualStatus.Success);
             Assert.NotNull(actualStatus.Errors);
             Assert.AreEqual(actualStatus.Errors[0], string.Format("File '{0}' already exists.", expectedDataSourceItemFileName));
         }
 
+        /// <summary>
+        /// Test saving a DataSourceItem where the item parameter is null.
+        /// </summary>
         [Test]
         public void SaveItem_NullItem()
         {
@@ -189,25 +208,31 @@ namespace SSRSMigrate.Tests.Exporter
             Assert.That(ex.Message, Is.EqualTo("Value cannot be null.\r\nParameter name: item"));
         }
 
+        /// <summary>
+        /// Test saving a DataSourceItem where the the filename parameter is null.
+        /// </summary>
         [Test]
         public void SaveItem_NullFileName()
         {
             ArgumentException ex = Assert.Throws<ArgumentException>(
                 delegate
                 {
-                    exporter.SaveItem(expectedDataSourceItem, null);
+                    exporter.SaveItem(dataSourceItem, null);
                 });
 
             Assert.That(ex.Message, Is.EqualTo("fileName"));
         }
 
+        /// <summary>
+        /// Test saving a DataSourceItem where the filename parameter is empty.
+        /// </summary>
         [Test]
         public void SaveItem_EmptyFileName()
         {
             ArgumentException ex = Assert.Throws<ArgumentException>(
                 delegate
                 {
-                    exporter.SaveItem(expectedDataSourceItem, "");
+                    exporter.SaveItem(dataSourceItem, "");
                 });
 
             Assert.That(ex.Message, Is.EqualTo("fileName"));
